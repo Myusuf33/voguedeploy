1636fc69abc51a1949c409a6fe6605a7aae3c0de
Hassan Shahid <your_email@example.com>
2025-01-02 10:40:49 -0800

    third commit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,29 @@
 
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       gridTemplateColumns:{
+//         'auto':'repeat(auto-fill, minmax(200px, 1fr))'
+//       },
+//       colors:{
+//         'primary':'#5F6FFF'
+//       }
+//     },
+//   },
+//   plugins: [ require('tailwind-scrollbar'),],
+// }
+
+
+
+
 /** @type {import('tailwindcss').Config} */
+import tailwindScrollbar from 'tailwind-scrollbar';
+
 export default {
   content: [
     "./index.html",
@@ -7,13 +31,13 @@ export default {
   ],
   theme: {
     extend: {
-      gridTemplateColumns:{
-        'auto':'repeat(auto-fill, minmax(200px, 1fr))'
+      gridTemplateColumns: {
+        'auto': 'repeat(auto-fill, minmax(200px, 1fr))',
       },
-      colors:{
-        'primary':'#5F6FFF'
-      }
+      colors: {
+        'primary': '#5F6FFF',
+      },
     },
   },
-  plugins: [ require('tailwind-scrollbar'),],
-}
+  plugins: [tailwindScrollbar],
+};
