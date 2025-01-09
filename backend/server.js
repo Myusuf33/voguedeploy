495e82ee1f492f58c7c415fd7050f73cbb217812
Hassan Shahid <106225281+Hassanshahid0@users.noreply.github.com>
2025-01-09 14:58:28 -0800

    Update server.js
    Signed-off-by: Hassan Shahid <106225281+Hassanshahid0@users.noreply.github.com>

--- backend/server.js
+++ backend/server.js
@@ -75,17 +75,7 @@ const corsOptions = {
   ],
   methods: ["POST", "GET", "PUT", "DELETE"],
   credentials: true,
-  allowedHeaders: [
-    "X-CSRF-Token", 
-    "X-Requested-With", 
-    "Accept", 
-    "Accept-Version", 
-    "Content-Length", 
-    "Content-MD5", 
-    "Content-Type", 
-    "Date", 
-    "X-Api-Version"
-  ],
+
 };
 
 
