e7a5d36f0fec9f07cf7b2ea6ba679a3499417efe
Hassan Shahid <106225281+Hassanshahid0@users.noreply.github.com>
2025-01-09 15:59:11 -0800

    Update vite.config.js
    Signed-off-by: Hassan Shahid <106225281+Hassanshahid0@users.noreply.github.com>

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -2,7 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server:{
+    
+//   }
+// })
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://voguedeploy.vercel.app',
+        changeOrigin: true, // Ensures the origin of the host header is changed to the target URL
+        secure: true,       // Ensures SSL is handled
+      },
+    },
+  },
   plugins: [react()],
-  server:{port:5174}
-})
+});
