7da02cf344841e9ffe0fbf45345e3457bc81cc8f
Hassan Shahid <106225281+Hassanshahid0@users.noreply.github.com>
2025-01-10 04:25:24 -0800

    Update Footer.jsx
    Signed-off-by: Hassan Shahid <106225281+Hassanshahid0@users.noreply.github.com>

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -68,8 +68,8 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>+1-212-456-7890</li>
-            <li>Voguevibe@gmail.com</li>
+            <li>03164356746</li>
+            <li> myousaf5431@gmail.com</li>
           </ul>
         </div>
 
