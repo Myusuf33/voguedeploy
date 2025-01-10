fea4243d524336a5d28fbb73cf0d6b48e799be97
Hassan Shahid <106225281+Hassanshahid0@users.noreply.github.com>
2025-01-09 17:42:48 -0800

    Update Footer.jsx
    Signed-off-by: Hassan Shahid <106225281+Hassanshahid0@users.noreply.github.com>

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,49 @@
+// import React from 'react'
+// import { assets } from '../assets/assets'
+
+// const Footer = () => {
+//   return (
+//     <div className='md:mx-10'>
+//       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+
+//         <div>
+//           <img className='mb-5 w-40' src={assets.logo} alt="" />
+//           <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+//         </div>
+
+//         <div>
+//           <p className='text-xl font-medium mb-5'>COMPANY</p>
+//           <ul className='flex flex-col gap-2 text-gray-600'>
+//             <li>Home</li>
+//             <li>About us</li>
+//             <li>Delivery</li>
+//             <li>Privacy policy</li>
+//           </ul>
+//         </div>
+
+//         <div>
+//           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+//           <ul className='flex flex-col gap-2 text-gray-600'>
+//             <li>+1-212-456-7890</li>
+//             <li>greatstackdev@gmail.com</li>
+//           </ul>
+//         </div>
+
+//       </div>
+
+//       <div>
+//         <hr />
+//         <p className='py-5 text-sm text-center'>Copyright 2024 @ Prescripto.com - All Right Reserved.</p>
+//       </div>
+
+//     </div>
+//   )
+// }
+
+// export default Footer
 import React from 'react'
 import { assets } from '../assets/assets'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 const Footer = () => {
   return (
@@ -14,10 +58,10 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>COMPANY</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Delivery</li>
-            <li>Privacy policy</li>
+            <NavLink to='/'><li className='cursor-pointer hover:text-black'>Home</li></NavLink>
+            <NavLink to='/barber'><li className='cursor-pointer hover:text-black'>My Shopes</li></NavLink>
+            <NavLink to='/about'><li className='cursor-pointer hover:text-black'>About</li></NavLink>
+            <NavLink to='/contact'><li className='cursor-pointer hover:text-black'>Contact</li></NavLink>
           </ul>
         </div>
 
@@ -25,7 +69,7 @@ const Footer = () => {
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
             <li>+1-212-456-7890</li>
-            <li>greatstackdev@gmail.com</li>
+            <li>Voguevibe@gmail.com</li>
           </ul>
         </div>
 
@@ -33,7 +77,7 @@ const Footer = () => {
 
       <div>
         <hr />
-        <p className='py-5 text-sm text-center'>Copyright 2024 @ Prescripto.com - All Right Reserved.</p>
+        <p className='py-5 text-sm text-center'>Copyright 2024 @ Voguevibe.com - All Right Reserved.</p>
       </div>
 
     </div>
