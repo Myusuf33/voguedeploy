06bcba151ea56a7fcb05e320701f8ec49c6cd72e
Hassan Shahid <106225281+Hassanshahid0@users.noreply.github.com>
2025-01-09 16:24:42 -0800

    Update App.jsx
    Signed-off-by: Hassan Shahid <106225281+Hassanshahid0@users.noreply.github.com>

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,3 +1,54 @@
+// import React, { useContext } from 'react'
+// import { DContext } from './context/DContext';
+// import { AdminContext } from './context/AdminContext';
+// import { Route, Routes } from 'react-router-dom'
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import Navbar from './components/Navbar'
+// import Sidebar from './components/Sidebar'
+// import Dashboard from './pages/Admin/Dashboard';
+// import AllAppointments from './pages/Admin/AllAppointments';
+// import AddD from './pages/Admin/AddD';
+// import Saloonlist from './pages/Admin/Saloonlist';
+// import Login from './pages/Login';
+// import DAppointments from './pages/Saloon/DAppointments';
+// import DDashboard from './pages/Saloon/DDashboard';
+// import DProfile from './pages/Saloon/DProfile';
+
+// const App = () => {
+
+//   const { dToken } = useContext(DContext)
+//   const { aToken } = useContext(AdminContext)
+
+//   return dToken || aToken ? (
+//     <div className='bg-[#F8F9FD]'>
+//       <ToastContainer />
+//       <Navbar />
+//       <div className='flex items-start'>
+//         <Sidebar />
+//         <Routes>
+//           <Route path='/' element={<></>} />
+//           <Route path='/admin-dashboard' element={<Dashboard />} />
+//           <Route path='/all-appointments' element={<AllAppointments />} />
+//           <Route path='/add-Saloon' element={<AddD />} />
+//           <Route path='/saloon-list' element={<Saloonlist />} />
+//           <Route path='/saloon-dashboard' element={<DDashboard />} />
+//           <Route path='/saloon-appointments' element={<DAppointments />} />
+//           <Route path='/saloon-profile' element={<DProfile />} />
+//         </Routes>
+//       </div>
+//     </div>
+//   ) : (
+//     <>
+//       <ToastContainer />
+//       <Login />
+//     </>
+//   )
+// }
+
+// export default App
+
+
 import React, { useContext } from 'react'
 import { DContext } from './context/DContext';
 import { AdminContext } from './context/AdminContext';
